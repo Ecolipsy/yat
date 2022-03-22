f2d42c06e4f7faee1bd4530d7eca20a88f5e568f
Ecolipsy <ecolipsy@outlook.com>
2022-03-22 22:08:24 +0100

    Add crossprotocol support

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ yargs.option("sourcehost", {description: "The source of the tunnel. (Server host
 yargs.option("destinationhost", {description: "The destination of the tunnel. (Listening host)", alias: "dhost"});
 yargs.option("sourceport", {description: "The source of the tunnel. (Server port)", alias: "sport"});
 yargs.option("destinationport", {description: "The destination of the tunnel. (Listening port)", alias: "dport"});
+yargs.option("destinationprotocol", {description: "The protocol of the destination. (Listening protocol)", alias: "dprot"});
 yargs.argv;
 //console.log(yargs.argv);
 if(!yargs.argv["protocol"]){
@@ -47,25 +48,35 @@ if(isNaN(yargs.argv["destinationport"])){
     console.log("For help, execute 'yat --help'");
     process.exit();
 }
+var dprot = yargs.argv["destinationprotocol"] || yargs.argv["protocol"];
 
 var shost = yargs.argv["sourcehost"];
 var sport = yargs.argv["sourceport"];
 var dhost = yargs.argv["destinationhost"];
 var dport = yargs.argv["destinationport"];
 
-switch(yargs.argv.protocol.toLowerCase()){
+switch(dprot.toLowerCase()){
     case "tcp":
         const server = tcp.createServer();
         
         server.on("connection", (client) => {
             console.log("Connection from", client.remoteAddress + ":" + client.remotePort);
-            client._client = tcp.createConnection({host: shost, port: sport});
-            client.on("data", (d) => {
-                client._client.write(d);
-            });
-            client._client.on("data", (d) => {
-                client.write(d);
-            });
+            if(yargs.argv["protocol"] == "tcp"){
+                client._client = tcp.createConnection({host: shost, port: sport});
+                client.on("data", (d) => {
+                    client._client.write(d);
+                });
+                client._client.on("data", (d) => {
+                    client.write(d);
+                });
+            } else{
+                client.on("data", (d) => {
+                    var udpclient = udp.createSocket(yargs.argv["protocol"], (clb, rinfo) => {
+                        client.write(clb);
+                    });
+                    udpclient.send(d, sport, shost);
+                });
+            }
         });
 
         server.on("listening", () => {
@@ -77,10 +88,20 @@ switch(yargs.argv.protocol.toLowerCase()){
     case "udp4":
         const sock = udp.createSocket("udp4", (d, ri) => {
             console.log("Connection from", ri.address + ":" + ri.port);
-            var rSock = udp.createSocket("udp4", (rD, rRi) => {
-                sock.send(rD, ri.port, ri.address);
-            });
-            rSock.send(d, sport, shost);
+            if(yargs.argv["protocol"].toLowerCase().startsWith("udp")){
+                var rSock = udp.createSocket(yargs.argv["protocol"], (rD, rRi) => {
+                    sock.send(rD, ri.port, ri.address);
+                });
+                rSock.send(d, sport, shost);
+            } else{
+                var cli = tcp.createConnection({host: shost, port: sport});
+                var ssock = udp.createSocket("udp4", (clb, sri) => {
+                    cli.write(clb);
+                });
+                cli.on("data", (d) => {
+                    ssock.send(d, ri.port, ri.address);
+                });
+            }
         });
         sock.bind(dport, dhost);
         sock.on("listening", () => {
@@ -90,17 +111,27 @@ switch(yargs.argv.protocol.toLowerCase()){
     case "udp6":
         const sock6 = udp.createSocket("udp6", (d, ri) => {
             console.log("Connection from", ri.address + ":" + ri.port);
-            var rSock = udp.createSocket("udp6", (rD, rRi) => {
-                sock6.send(rD, ri.port, ri.address);
-            });
-            rSock.send(d, sport, shost);
+            if(yargs.argv["protocol"].toLowerCase().startsWith("udp")){
+                var rSock = udp.createSocket(yargs.argv["protocol"], (rD, rRi) => {
+                    sock6.send(rD, ri.port, ri.address);
+                });
+                rSock.send(d, sport, shost);
+            } else{
+                var cli = tcp.createConnection({host: shost, port: sport});
+                var ssock = udp.createSocket("udp6", (clb, sri) => {
+                    cli.write(clb);
+                });
+                cli.on("data", (d) => {
+                    ssock.send(d, ri.port, ri.address);
+                });
+            }
         });
         sock6.bind(dport, dhost);
         sock6.on("listening", () => {
             console.log("(UDP) Listening to", dhost + ":" + dport);
         });
         break;
-    
+
     default:
         console.log(`Unknown protocol '${yargs.argv.protocol}'`);
         console.log("For help, execute 'yat --help'");
